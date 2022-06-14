db42b10d7b4359760434db66315105b7d7bd14f9
andirachmadian <andikharisma24@gmail.com>
2022-06-15 05:54:55 +0700

    add button increase age

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useState , useEffect } from "react"
 import './App.css';
 import {db} from './firebase-config'
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, addDoc } from "firebase/firestore"
 
 function App() {
+  const [newName, setNewName] = useState("")
+  const [newAge, setNewAge] = useState(0)
   const [users, setUsers] = useState([])
   const usersCollectionRef = collection(db, "users")
 
   const createUser = async () => {
-
+    await addDoc(usersCollectionRef, {name : newName, age: newAge})
   }
 
   useEffect(() => {
@@ -24,8 +26,8 @@ function App() {
 
   return (
     <div className="App">
-      <input placeholder="Name..." />
-      <input placeholder="Age" type="number" />
+      <input placeholder="Name..." onChange={(event) => {setNewName(event.target.value)}}/>
+      <input placeholder="Age" type="number" onChange={(event) => {setNewAge(event.target.value)}} />
 
       <button onClick={createUser}> Create User </button>
       {users.map((user) => {
@@ -34,6 +36,7 @@ function App() {
             {""}
             <h1>Name: {user.name}</h1>
             <h1>Age: {user.age}</h1>
+            <button> Increase Age </button>
           </div>
         )
       })}
